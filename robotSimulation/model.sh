760fa4734af6684f8df3c3090fa41711269a99ec
ChenYing Kuo <evshary@gmail.com>
2021-07-26 11:02:09 +0800

    Rename Gazebo models

--- robotSimulation/model.sh
+++
@@ -1,7 +0,0 @@
-## Get your shell automaitcally
-shell=`echo $SHELL | awk -F '/' '{print $NF}'`
-
-export TURTLEBOT3_MODEL=waffle_pi
-export ROS_MASTER_URI=http://localhost:11311
-source simulation_ws/devel/setup.$shell
-unset ROS_IP
